70c9faa9c0acbdd39ba1e4c861b5156d53499444
m.izadi <mohammadreza.i1997@gmail.com>
2022-11-15 14:19:13 +0330

    config cp file

--- cp-file.sh
+++ cp-file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-file=`ls -t /srv/osm_generator/osm_export/export_data | head -1`
+file=`ls -t /opt/osm_generator/osm_export/export_data/| head -1`
 echo $file
-ls -t /srv/osm_generator/osm_export/export_data | head -1
-rsync -avzhe 'ssh -o StrictHostKeyChecking=no' /srv/osm_generator/osm_export/export_data/"$file" support@192.168.178.30:/etc/nginx/download/
+echo "################# Start Transfer File #################"
+ssh support@192.168.178.45 -o ConnectTimeout=15 rsync -avzhe 'ssh -o StrictHostKeyChecking=no' /srv/osm_generator/osm_export/export_data/$file support@192.168.178.30:/etc/nginx/download/
