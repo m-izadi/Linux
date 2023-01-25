786e74654d3c37d2df97d87d230595dfb3062a84
m.izadi <mohammadreza.i1997@gmail.com>
2023-01-25 16:07:16 +0330

    bashe

---
+++ Bash/Faranesh/6_for.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+clear
+# for number in {1..9};do
+# echo $number
+# done
+
+#or sintax #C
+
+# for ((i=0;i<=100;i+=5));do
+#     echo $i
+# done
+
+# for i in {0..100..5};do
+#     echo $i
+# done
+
+#______________________________________________________
+# IFS : Internal field seperator
+IFS=":"
+name="Mohammad:Erfun:Darya:Fatemeh"
+for i in $name ;do
+    echo hello $￼matin
+￼
+third-party
+vrp
+Jobs
+#60213
+ running Job build triggered just now by 
+￼
+MohammadReza Izadi
+￼
+￼
+1Running with gitlab-runner 15.5.1 (7178588d)
+2  on Shared Runner - Deliver on 7.221 yp9dTuPK
+3
+Preparing the "docker" executor
+00:04
+4Using Docker executor with image docker-virtual.localhost:8082/docker-virtual/toolskit:v2 ...
+5Authenticating with credentials from /root/.docker/config.json
+6Pulling docker image docker-virtual.localhost:8082/docker-virtual/toolskit:v2 ...
+7Using docker image sha256:f2068e7924357dafa7faff465a72f2fff6fa5bff3c371b848a4fc9e26d089faa for docker-virtual.localhost:8082/docker-virtual/toolskit:v2 with digest docker-virtual.localhost:8082/docker-virtual/toolskit@sha256:e11e823132807e3938c9b8115b463a75dcb8e838d572575c6b5e4da6b539fc42 ...
+9
+Preparing environment
+00:01
+10Running on runner-yp9dtupk-project-474-concurrent-0 via runner...
+12
+Getting source from Git repository
+13Fetching changes with git depth set to 20...
+14Reinitialized existing Git repository in /builds/matin/routaa/third-party/vrp/.git/
+15Checking out 4e03c273 as Stage...
+16Removing client-hc/target/
+17Removing core/target/
+18Removing hmm-lib/target/
+19Removing map-matching/target/
+20Removing reader-gtfs/target/
+21Removing tools/target/
+22Removing traffic/target/
+23Removing vrp/target/
+24Removing web-api/target/
+25i
+done
