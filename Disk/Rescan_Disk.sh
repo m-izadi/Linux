8a726355e055412293496858db8c1309eadcfd46
1402804 <1402804@fahm.kavosh.org>
2024-02-28 11:33:34 +0330

    rescan disk

--- Disk/Rescan_Disk.sh
+++ Disk/Rescan_Disk.sh
@@ -3,5 +3,11 @@ for host in $(ls /sys/class/scsi_host) ; do echo ${host}; echo "- - -" > /sys/cl
 
 echo 1 > /sys/block/sda/device/rescan
 
+#To scan for new disks :
+echo "- - -" | sudo tee /sys/class/scsi_host/host*/scan >/dev/null
+
+#To rescan existing disks :
+echo 1 | sudo tee /sys/class/block/sd?/device/rescan >/dev/null
+
 # sudo chmod +x Rescan_Disk.sh
 # sudo ./Disk.sh
